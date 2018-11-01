b9e05b228724ed5794407f3032da9ab68c11f101
justjavac <justjavac@gmail.com>
2018-11-01 10:04:42 +0800

    custom domain

--- .eleventy.js
+++ .eleventy.js
@@ -107,6 +107,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/.webmanifest');
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
   eleventyConfig.addPassthroughCopy('src/robots.txt');
+  eleventyConfig.addPassthroughCopy('src/CNAME');
   eleventyConfig.addPassthroughCopy('src/_img');
 
   return {
