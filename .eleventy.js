dec6fdcd96b3b939c95a425ed29e5a618a186c61
justjavac <justjavac@gmail.com>
2018-11-01 10:23:58 +0800

    turn off Jekyll

--- .eleventy.js
+++ .eleventy.js
@@ -107,6 +107,7 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
   eleventyConfig.addPassthroughCopy('src/robots.txt');
   eleventyConfig.addPassthroughCopy('src/CNAME');
+  eleventyConfig.addPassthroughCopy('src/.nojekyll');
   eleventyConfig.addPassthroughCopy('src/_img');
 
   return {
