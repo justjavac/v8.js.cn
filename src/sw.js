9c6766a2ed3b659a00127e013721c549e23d84e9
迷渡 <justjavac@gmail.com>
2019-06-14 14:47:51 +0800

    upgrade workbox v4.3.1

--- src/sw.js
+++ src/sw.js
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-importScripts('https://cdn.devtips.cn/workbox-cdn/releases/4.0.0-alpha.0/workbox-sw.js');
+importScripts('https://cdn.devtips.cn/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 workbox.core.setCacheNameDetails({
   prefix: 'v8.js.cn',
